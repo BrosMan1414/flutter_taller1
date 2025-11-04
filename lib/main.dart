6a64d9c332e56598d14c62e38f6c66f2fa3de9dc
Mario Ochoa Arango <118146989+BrosMan1414@users.noreply.github.com>
2025-11-04 17:48:43 -0500

    integracion de Firebase y creacion y gestion de la colección universidades

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_taller1/routes/app_router.dart';
 import 'package:flutter_taller1/themes/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'modules/jwt/providers/auth_provider.dart';
 import 'modules/jwt/services/auth_service.dart';
 // removed demo/fake auth usage per request
 import 'modules/jwt/services/storage_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
